2c03527b9a4f5bdd8c5687a23c2fb7ec4bbe52c0
Kevin Wilbert Johan <134535621+kyzn-15@users.noreply.github.com>
2025-03-26 02:02:37 +0700

    changing app.py to main.py for deployment

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
 # Load the module dynamically (e.g., main.py or passenger_wsgi.py)
-module_name = 'app'  # Replace with 'passenger_wsgi' if needed
+module_name = 'main'  # Replace with 'passenger_wsgi' if needed
 module_path = os.path.join(os.path.dirname(__file__), f'{module_name}.py')
 
 spec = importlib.util.spec_from_file_location(module_name, module_path)
